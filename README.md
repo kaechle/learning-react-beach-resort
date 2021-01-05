5bca1272f5aaefe4a308c49e8df646048d5f58cd
Tim Kaechle <timothy.kaechle@me.com>
2021-01-04 22:26:19 -0300

    completed navbar

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Learing React: Beach Resort 
+# Learning React: Beach Resort 
 
 A project for learning React using [this](https://www.youtube.com/watch?v=LXJOvkVYQqA) tutorial.
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../images/logo.svg';
-import { FaAlignRight } from 'react-icons';
+import { FaAlignRight } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
@@ -18,7 +18,18 @@ export default class Navbar extends Component {
             <Link to="/">
               <img src={ logo } alt="Beach Resort"/>
             </Link>
+            <button type="button" className="nav-btn" onClick={this.handleToggler}>
+              <FaAlignRight className="nav-icon" />
+            </button>
           </div>
+          <ul className={this.state.isOpen?"nav-links show-nav":"nav-links"}>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/rooms">Rooms</Link>
+            </li>
+          </ul>
         </div>
       </nav>
     )
